746a8e767b36d9270f66f0d8d710767141990c46
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-08 05:43:10 +0000

    feat: handle pipeline arguments

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 export { assertEquals } from "https://deno.land/std@0.191.0/testing/asserts.ts";
-import Client from "https://sdk.fluentci.io/z1/mod.ts";
+import Client from "https://sdk.fluentci.io/v0.2.0/mod.ts";
 export default Client;
 
-export { connect, uploadContext } from "https://sdk.fluentci.io/z1/mod.ts";
+export { connect, uploadContext } from "https://sdk.fluentci.io/v0.2.0/mod.ts";
 export { brightGreen } from "https://deno.land/std@0.191.0/fmt/colors.ts";
-export { withDevbox } from "https://nix.fluentci.io/zenith/src/dagger/steps.ts";
+export { withDevbox } from "https://nix.fluentci.io/v0.5.2/src/dagger/steps.ts";
 export { stringifyTree } from "https://esm.sh/stringify-tree@1.1.1";
 import gql from "https://esm.sh/graphql-tag@2.12.6";
 export { gql };
@@ -21,6 +21,8 @@ export {
   join,
   resolve,
 } from "https://deno.land/std@0.203.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.205.0/flags/mod.ts";
+export { snakeCase, camelCase } from "https://cdn.skypack.dev/lodash";
 
 export * as FluentGitlabCI from "https://deno.land/x/fluent_gitlab_ci@v0.4.2/mod.ts";
 export * as FluentGithubActions from "https://deno.land/x/fluent_github_actions@v0.2.1/mod.ts";
