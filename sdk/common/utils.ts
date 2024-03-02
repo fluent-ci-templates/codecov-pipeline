aec002d0128602417c4b4a44fe45540168c58462
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-03-02 09:50:43 +0000

    fix missing version for 'npm:node-color-log'

--- sdk/common/utils.ts
+++ sdk/common/utils.ts
@@ -1,4 +1,4 @@
-import logger from "npm:node-color-log";
+import logger from "npm:node-color-log@11.0.2";
 
 export const log = (stack?: string) =>
   logger.bgColor("red").color("black").log(stack);
