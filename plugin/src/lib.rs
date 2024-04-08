08a468d1da97ec72a071abe4cb34e4b76f55353c
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-08 05:09:03 +0000

    [plugin] use fluentci-pdk v0.1.9

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -6,16 +6,14 @@ pub fn upload(args: String) -> FnResult<String> {
     let stdout = dag()
         .pipeline("upload")?
         .pkgx()?
-        .with_exec(vec!["pkgx", "install", "curl", "git"])?
+        .with_packages(vec!["curl", "git"])?
         .with_exec(vec![
             "curl",
             "-Os",
             "https://uploader.codecov.io/latest/linux/codecov",
         ])?
         .with_exec(vec!["chmod", "+x", "codecov"])?
-        .with_exec(vec!["mkdir", "-p", ".local/bin"])?
-        .with_exec(vec!["mv", "codecov", ".local/bin/codecov"])?
-        .with_exec(vec![".local/bin/codecov", "-t", "$CODECOV_TOKEN", &args])?
+        .with_exec(vec!["./codecov", "-t", "$CODECOV_TOKEN", &args])?
         .stdout()?;
     Ok(stdout)
 }
